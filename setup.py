6977a2b80eb7d061755817622d2688499fcb4e5b
Harsh Vardhan Singh <singh.har@husky.neu.edu>
2023-08-07 13:38:12 -0400

    Update library version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ CLASSIFIERS = [
 
 # calling the setup function
 setuptools.setup(name='zi_api_auth_client',
-                 version='2.0.0',
+                 version='2.0.1',
                  description='A library that supports username-password and PKI authentication methods for enterprise-api',
                  long_description=long_description,
                  url='https://github.com/Zoominfo/api-auth-python-client',
